5b9a719386cec9bc8b7e9be79be945bbebf0d4fb
ChaseCollins22 <chasecollins1954@gmail.com>
2023-11-04 12:38:47 -0700

    remove duplicate files

---
+++ part2/part2-notes-frontend/src/App.jsx
@@ -0,0 +1,90 @@
+import { useState, useEffect } from 'react'
+import noteService from './services/notes'
+import Footer from './components/Footer'
+import Note from './components/Note'
+import Notification from './components/ErrorNotification'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(allNotes => setNotes(allNotes))
+  }, []);
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(createdNote => {
+        setNotes([...notes, createdNote])
+        setNewNote('')
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((note) => note.id === id)
+    const modifiedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, modifiedNote)
+      .then(updatedNote => {
+        setNotes(notes.map(note =>
+          note.id === id
+            ? updatedNote
+            : note
+        ))
+      })
+      .catch(error => {
+        setErrorMessage(`The note, '${note.content}' has already been removed from the server`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000);
+        setNotes(notes.filter(note => note.id !== id))
+      })
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ part3/phonebook-backend/index 2.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+
+const morgan = require('morgan');
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(cors())
+app.use(express.static('dist'))
+
+const MAX_CONTACTS = 500;
+
+let contacts = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+const generateId = () => {
+  return Math.floor(Math.random() * MAX_CONTACTS)
+}
+
+app.get('/api/persons', (request, response) => {
+  response.json(contacts)
+})
+
+app.get('/info', (request, response) => {
+  const totalContacts = contacts.length
+  const dateNow = new Date().toString()
+  response.send(`<p>Phonebook has info for ${totalContacts} people</p><p>${dateNow}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const contact = contacts.find(contact => contact.id === id)
+
+  if (contact) response.json(contact)
+
+  return response.status(404).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const contact = contacts.find(contact => contact.id === id);
+
+  if (contact) {
+    contacts = contacts.filter(contact => contact.id !== id)
+    response.status(204).end()
+  } else {
+    response.statusMessage = 'Contact not found'
+    response.status(404).end()
+  }
+})
+
+app.post('/api/persons', (request, response) => {
+  if (!request.body.name || !request.body.number) {
+    return response.status(400).json({
+      error: 'Name or number missing'
+    })
+  }
+
+  const contactExists = contacts.find(contact => {
+    return (contact.name === request.body.name &&
+           contact.number === request.body.number)
+  })
+
+  if (contactExists) return response.status(400).json({
+    error: 'Contact already exists'
+  })
+
+  const newContact = {
+    id: generateId(),
+    name: request.body.name,
+    number: request.body.number
+  }
+
+  contacts = [...contacts, newContact]
+
+  response.json(newContact)
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
